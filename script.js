124c019e64d03deba89d8df1fca3d462fa901079
HanselEsp <69163352+HanselEsp@users.noreply.github.com>
2021-07-22 09:57:00 -0700

    I did the changes

--- script.js
+++ script.js
@@ -2,5 +2,19 @@ function compute()
 {
     p = document.getElementById("principal").value;
     
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    
 }
-        
+function SliderValue()
+{
+    var slider = document.getElementById("rate");
+    var output = document.getElementById("rate_display");
+    output.innerHTML = slider.value; 
+
+    
+    slider.oninput = function() 
+    {
+        output.innerHTML = this.value;
+    }  
+}    
